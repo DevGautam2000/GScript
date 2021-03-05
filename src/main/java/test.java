d9bbfad96a8134dbc04ea19353db9fcf66530b13
Gautam <devgautam1231@gmail.com>
2021-03-05 15:46:13 +0530

    Added new functions

---
+++ src/main/java/test.java
@@ -0,0 +1,17 @@
+
+import static gscript.Gio.*;
+import static gscript.NewMath.*;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        //for printing hello world
+        sout("Hello World"); // ------------>  gscript()
+
+        // for printing hello world
+        System.out.print("Hello World"); // ------------>  java
+
+
+    }
+}
